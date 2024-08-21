b41ae88cc49837113ba9aba54289411472f7b1e1
mroluwasesan <mr.oluwasesan@gmail.com>
2024-08-21 14:07:53 +0100

    test

--- src/Bg.Web/Controllers/HomeController.cs
+++ src/Bg.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace Bg.Web.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Environment = "green"; // Change to "Green" in the Green environment
+            ViewBag.Environment = "blue"; // Change to "Green" in the Green environment
             return View();
         }
     }
